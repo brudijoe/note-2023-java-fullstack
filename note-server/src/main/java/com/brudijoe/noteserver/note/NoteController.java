e6bc214e6f44c50469dbff18e886f932d8f92f32
Speckens <ingo.speckens@encoway.de>
2023-08-04 12:24:23 +0200

    Add_crossOrigin_annotation

--- note-server/src/main/java/com/brudijoe/noteserver/note/NoteController.java
+++ note-server/src/main/java/com/brudijoe/noteserver/note/NoteController.java
@@ -3,6 +3,7 @@ package com.brudijoe.noteserver.note;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1/note")
+@CrossOrigin
 public class NoteController {
 
     private final NoteService noteService;
