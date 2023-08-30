dfc0261ae2d7622ae00e5c17f0e93b1996c56760
Ingo Speckens <doningo@ymail.com>
2023-08-30 10:00:17 +0200

    Improve_modal_and_improve_backdrop

--- note-client/src/components/Dialog.tsx
+++ note-client/src/components/Dialog.tsx
@@ -6,7 +6,7 @@ interface DialogProps {
 export function Dialog({dialogRef, children}: DialogProps) {
   return (
     <dialog
-      className="w-72 p-4 bg-gray-200 dark:bg-gray-800 border-2 border-black dark:border-white rounded"
+      className="w-72 p-4 backdrop:bg-gray-800 backdrop:bg-opacity-80 dark:backdrop:bg-gray-900 dark:backdrop:bg-opacity-80 bg-gray-200 dark:bg-gray-800 border-2 border-black dark:border-white rounded"
       ref={dialogRef}
     >
       {children}
