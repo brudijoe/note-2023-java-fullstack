a5100ab741200c4a8d91f761fa42850b46864981
Ingo Speckens <doningo@ymail.com>
2023-09-03 16:15:24 +0200

    Refactor_initial_loading_screen

--- note-client/src/App.tsx
+++ note-client/src/App.tsx
@@ -4,26 +4,34 @@ import useNotes, {NoteContext} from "./hooks/useNotes";
 import {CreateNote} from "./pages/CreateNote";
 import {NoteCard} from "./pages/NoteCard";
 import {Header} from "./pages/Header";
+import {Title} from "./components/Title";
 
 function App() {
   const {notes, loading, error, addNote, deleteNote, editNote} = useNotes();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
 
   return (
     <NoteContext.Provider value={{notes, loading, error, addNote, deleteNote, editNote}}>
       <div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-900">
         <Header />
-        <CreateNote />
-        <div className="flex flex-row flex-wrap p-4 gap-4">
-          {notes && map((singleNote) => <NoteCard key={singleNote.id} singleNote={singleNote} />, notes)}
-        </div>
+        {loading ? (
+          <div className="text-black dark:text-white font-bold">Loading...</div>
+        ) : error ? (
+          <Title title={error} />
+        ) : (
+          notes && (
+            <>
+              <CreateNote />
+              <div className="flex flex-row flex-wrap p-4 gap-4">
+                {map(
+                  (singleNote) => (
+                    <NoteCard key={singleNote.id} singleNote={singleNote} />
+                  ),
+                  notes
+                )}
+              </div>
+            </>
+          )
+        )}
       </div>
     </NoteContext.Provider>
   );
