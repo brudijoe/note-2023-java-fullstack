2c4e3c9e25296aa87f5996b7430076761b201fe6
Ingo Speckens <doningo@ymail.com>
2023-09-05 13:48:36 +0200

    Fix_typescript

--- note-client/src/main.tsx
+++ note-client/src/main.tsx
@@ -3,7 +3,7 @@ import {createRoot} from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root")!);
 root.render(
   <StrictMode>
     <App />
