236d08fdf5c6113bef25c5c4560640f41dbbf4f8
Speckens <doningo@ymail.com>
2023-09-18 15:05:33 +0200

    Add_dockerfile

--- note-client/vite.config.ts
+++ note-client/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Fix for windows hot reload
+  watch: {
+    usePolling: true,
+  },
+  // Needed for the Docker Container port mapping to work
+  host: true,
+  strictPort: true,
+  port: 5173,
 })
